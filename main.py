03d286da10161c23683c51a3ea5097f6ec7d0129
tanakatsubasa5010 <tanakatsubasa0105@icloud.com>
2024-12-12 12:22:14 +0900

    main

--- main.py
+++ main.py
@@ -7,48 +7,21 @@ if __name__ == '__main__':
     dataset = CrossValidationDataset(r"C:\Users\tanaka\dataset\cross_validation\detection\3bacteria", 3, 1, 1, augment=['identity'])
     for data, test in dataset:
         torch.cuda.empty_cache()
-        model = YOLO('sod-ema-yolo11n.yaml')
-        model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11n')
+        model = YOLO('yolov8n.yaml')
+        model.train(data=data, batch=8, epochs=500, patience=500, workers=0, name='yeast_only_yolov8n')
         
         torch.cuda.empty_cache()
-        model = YOLO('sod-ema-yolo11s.yaml')
-        model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11s')
+        model = YOLO('yolov8s.yaml')
+        model.train(data=data, batch=8, epochs=500, patience=500, workers=0, name='yeast_only_yolov8s')
         
         torch.cuda.empty_cache()
-        model = YOLO('sod-ema-yolo11m.yaml')
-        model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11m')
+        model = YOLO('yolov8m.yaml')
+        model.train(data=data, batch=8, epochs=500, patience=500, workers=0, name='yeast_only_yolov8m')
         
         torch.cuda.empty_cache()
-        model = YOLO('sod-ema-yolo11l.yaml')
-        model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11l')
+        model = YOLO('yolov8l.yaml')
+        model.train(data=data, batch=8, epochs=500, patience=500, workers=0, name='yeast_only_yolov8l')
         
         torch.cuda.empty_cache()
-        model = YOLO('sod-ema-yolo11x.yaml')
-        model.train(data=data, batch=2, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11x')
-
-
-
-        torch.cuda.empty_cache()
-        model = YOLO('yolo11n.yaml')
-        model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11n')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('yolo11s.yaml')
-        model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11s')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('yolo11m.yaml')
-        model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11m')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('yolo11l.yaml')
-        model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11l')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('yolo11x.yaml')
-        model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11x')
-
-
-
-
-        
+        model = YOLO('yolov8x.yaml')
+        model.train(data=data, batch=8, epochs=500, patience=500, workers=0, name='yeast_only_yolov8x')
