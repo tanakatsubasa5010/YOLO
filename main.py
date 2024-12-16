b32748ef39a8dc81afc768710548ba14b8d65f00
tanakatsubasa5010 <tanakatsubasa0105@icloud.com>
2024-12-16 14:02:18 +0900

    ちょっとだけ変更

--- main.py
+++ main.py
@@ -3,50 +3,80 @@ from ultralytics import YOLO
 import torch
 
 if __name__ == '__main__':
-    epochs = 1
+    epochs = 500
     dataset = CrossValidationDataset(r"C:\Users\tanaka\dataset\cross_validation\detection\3bacteria", 3, 1, 1, augment=['identity'])
     for data, test in dataset:
-        torch.cuda.empty_cache()
-        model = YOLO('sod-ema-yolo11n.yaml')
-        model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11n')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('sod-ema-yolo11s.yaml')
-        model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11s')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('sod-ema-yolo11m.yaml')
-        model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11m')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('sod-ema-yolo11l.yaml')
-        model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11l')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('sod-ema-yolo11x.yaml')
-        model.train(data=data, batch=2, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11x')
+        try:
+            torch.cuda.empty_cache()
+            model = YOLO('sod-ema-yolo11n.yaml')
+            model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11n')
+        except:
+            pass
+
+        try:
+            torch.cuda.empty_cache()
+            model = YOLO('sod-ema-yolo11s.yaml')
+            model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11s')
+        except:
+            pass
+
+        try:
+            torch.cuda.empty_cache()
+            model = YOLO('sod-ema-yolo11m.yaml')
+            model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11m')
+        except:
+            pass
+
+        try:
+            torch.cuda.empty_cache()
+            model = YOLO('sod-ema-yolo11l.yaml')
+            model.train(data=data, batch=4, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11l')
+        except:
+            pass
+
+        try:
+            torch.cuda.empty_cache()
+            model = YOLO('sod-ema-yolo11x.yaml')
+            model.train(data=data, batch=2, epochs=epochs, patience=epochs, workers=0, name='sod-ema-yolo11x')
+        except:
+            pass
 
 
 
-        torch.cuda.empty_cache()
-        model = YOLO('yolo11n.yaml')
-        model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11n')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('yolo11s.yaml')
-        model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11s')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('yolo11m.yaml')
-        model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11m')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('yolo11l.yaml')
-        model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11l')
-        
-        torch.cuda.empty_cache()
-        model = YOLO('yolo11x.yaml')
-        model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11x')
+        try:
+            torch.cuda.empty_cache()
+            model = YOLO('yolo11n.yaml')
+            model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11n')
+        except:
+            pass
+
+        try:
+            torch.cuda.empty_cache()
+            model = YOLO('yolo11s.yaml')
+            model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11s')
+        except:
+            pass
+
+        try:
+            torch.cuda.empty_cache()
+            model = YOLO('yolo11m.yaml')
+            model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11m')
+        except:
+            pass
+
+        try:
+            torch.cuda.empty_cache()
+            model = YOLO('yolo11l.yaml')
+            model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11l')
+        except:
+            pass
+
+        try:
+            torch.cuda.empty_cache()
+            model = YOLO('yolo11x.yaml')
+            model.train(data=data, batch=8, epochs=epochs, patience=epochs, workers=0, name='yolo11x')
+        except:
+            pass
 
 
 
--- val.py
+++ val.py
@@ -7,117 +7,116 @@ if __name__ == '__main__':
     data = r"C:\Users\tanaka\dataset\cross_validation\detection\3bacteria\temp0\test.yaml"
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11n\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11n\weights\best.pt')
     model.val(data=data, workers=0)
     
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11s\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11s\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11m\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11m\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11l\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11l\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11x\weights\best.pt')
-    model.val(data=data, workers=0)
-
+    model = YOLO(r'runs\detect\yolo11x\weights\best.pt')
+    model.val(data=data, batch=2, workers=0)
 
 
     data = r"C:\Users\tanaka\dataset\cross_validation\detection\3bacteria\temp1\test.yaml"
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11n2\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11n2\weights\best.pt')
     model.val(data=data, workers=0)
     
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11s2\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11s2\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11m2\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11m2\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11l2\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11l2\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11x2\weights\best.pt')
-    model.val(data=data, workers=0)
+    model = YOLO(r'runs\detect\yolo11x2\weights\best.pt')
+    model.val(data=data, batch=2, workers=0)
 
 
 
     data = r"C:\Users\tanaka\dataset\cross_validation\detection\3bacteria\temp2\test.yaml"
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11n3\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11n3\weights\best.pt')
     model.val(data=data, workers=0)
     
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11s3\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11s3\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11m3\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11m3\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11l3\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11l3\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11x3\weights\best.pt')
-    model.val(data=data, workers=0)
+    model = YOLO(r'runs\detect\yolo11x3\weights\best.pt')
+    model.val(data=data, batch=2, workers=0)
 
 
 
     data = r"C:\Users\tanaka\dataset\cross_validation\detection\3bacteria\temp3\test.yaml"
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11n4\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11n4\weights\best.pt')
     model.val(data=data, workers=0)
     
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11s4\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11s4\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11m4\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11m4\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11l4\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11l4\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11x4\weights\best.pt')
-    model.val(data=data, workers=0)
+    model = YOLO(r'runs\detect\yolo11x4\weights\best.pt')
+    model.val(data=data, batch=2, workers=0)
 
 
 
     data = r"C:\Users\tanaka\dataset\cross_validation\detection\3bacteria\temp4\test.yaml"
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11n5\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11n5\weights\best.pt')
     model.val(data=data, workers=0)
     
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11s5\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11s5\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11m5\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11m5\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11l5\weights\best.pt')
+    model = YOLO(r'runs\detect\yolo11l5\weights\best.pt')
     model.val(data=data, workers=0)
 
     torch.cuda.empty_cache()
-    model = YOLO(r'runs\detect\3bacteria\tla-yolo11x5\weights\best.pt')
-    model.val(data=data, workers=0)
+    model = YOLO(r'runs\detect\yolo11x5\weights\best.pt')
+    model.val(data=data, batch=2, workers=0)
